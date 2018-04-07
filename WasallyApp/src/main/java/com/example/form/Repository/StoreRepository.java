4a078252796a14b3f0bbb945f1521947864c166a
ali475 <32493519+ali475@users.noreply.github.com>
2018-04-07 12:53:05 +0200

    Add files via upload

--- WasallyApp/src/main/java/com/example/form/Repository/StoreRepository.java
+++ WasallyApp/src/main/java/com/example/form/Repository/StoreRepository.java
@@ -1,9 +1,19 @@
 package com.example.form.Repository;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.example.form.Entities.Store;
 
 public interface StoreRepository extends CrudRepository<Store, String>{
 
+	@Modifying(clearAutomatically = true)
+	@Transactional
+    @Query(value="UPDATE Store s SET s.State = :State WHERE s.Storename = :Storename")
+    public void updateState(@Param("State") boolean State, @Param("Storename") String Storename );		
+	
 }
--- WasallyApp/src/main/java/com/example/form/Repository/Store_productRepository.java
+++ WasallyApp/src/main/java/com/example/form/Repository/Store_productRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.example.form.Entities.Store_Product;
 
-public interface Store_productRepository extends CrudRepository<Store_Product,Integer >{
+public interface Store_productRepository extends CrudRepository<Store_Product,String >{
 
 }
