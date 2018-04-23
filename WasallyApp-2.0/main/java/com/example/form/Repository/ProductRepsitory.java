7fa32cd81ff85bef40d216c42eb99f150622c473
Abdelrahman Lotfy Mohammed <36973578+AbdoFCI@users.noreply.github.com>
2018-04-23 19:05:07 +0200

    Delete ProductRepsitory.java

--- WasallyApp-2.0/main/java/com/example/form/Repository/ProductRepsitory.java
+++
@@ -1,9 +0,0 @@
-package com.example.form.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.form.Entities.Product;
-
-public interface ProductRepsitory extends CrudRepository<Product, String>{
-
-}
