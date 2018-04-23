d35b06343080c991a82b85fb3a71fa3ad1ef852d
Abdelrahman Lotfy Mohammed <36973578+AbdoFCI@users.noreply.github.com>
2018-04-23 19:50:03 +0200

    Delete UserRepository.java

--- WasallyApp/src/main/java/com/example/form/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.form.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.form.Entities.User;
-
-public interface UserRepository extends CrudRepository<User, String>{
-
-}
