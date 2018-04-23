38115c91a29f495abb89e63376f4ba6b628366ca
Abdelrahman Lotfy Mohammed <36973578+AbdoFCI@users.noreply.github.com>
2018-04-23 19:05:04 +0200

    Delete LogProductRepository.java

--- WasallyApp-2.0/main/java/com/example/form/Repository/LogProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.form.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.form.Entities.LogProduct;
-
-public interface LogProductRepository extends CrudRepository<LogProduct,Integer>{
-
-}
