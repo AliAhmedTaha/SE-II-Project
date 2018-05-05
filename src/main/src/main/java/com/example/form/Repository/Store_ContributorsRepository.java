5a3716868061f3d262f839557275c1d04ef630de
Ali Ahmed <36967560+AliAhmedTaha@users.noreply.github.com>
2018-05-05 18:06:56 +0200

    Delete Store_ContributorsRepository.java

--- src/main/src/main/java/com/example/form/Repository/Store_ContributorsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.form.Repository;
-
-import org.springframework.data.repository.CrudRepository;
-import com.example.form.Entities.Store_Contributors;
-
-public interface Store_ContributorsRepository extends CrudRepository < Store_Contributors, String > {
-
-}
